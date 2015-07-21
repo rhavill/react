bfb54425e82069a18572345d44374db0075aa9f0
Ricardo Havill <rhavill@progresstesting.com>
2015-07-21 13:15:43 -0400

    Configured webpack hot module loading to avoid having to refresh browser.

---
+++ kanban_app/webpack.development.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  entry: [
+    path.resolve(ROOT_PATH, 'app/main.js')
+  ],
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+};
