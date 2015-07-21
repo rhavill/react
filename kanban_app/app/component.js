b7451c2e984ebb46534c20ea5e4079b52f73d6b0
Ricardo Havill <rhavill@progresstesting.com>
2015-07-21 13:58:45 -0400

    Merge dev and production config into a single config file.

--- kanban_app/app/component.js
+++ kanban_app/app/component.js
@@ -1,5 +1,5 @@
 module.exports = function () {
   var element = document.createElement('h1');
-  element.innerHTML = 'Hello worlds!';
+  element.innerHTML = 'Hello world!';
   return element;
 };
--- kanban_app/webpack.config.js
+++ kanban_app/webpack.config.js
@@ -1,10 +1,37 @@
 var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'app/main'),
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: [path.resolve(ROOT_PATH, 'app/main')],
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
 };
+
+// we'll extend these later and use merge then
+if(TARGET === 'build') {
+  module.exports = common;
+}
+
+if(TARGET === 'dev') {
+  module.exports = common;
+}
 
