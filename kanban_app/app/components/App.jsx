b1c180991a6f74d08484d85485a55c1a7d99092e
Ricardo Havill <rhavill@progresstesting.com>
2015-07-22 10:29:48 -0400

    Allow notes to be deleted.

--- kanban_app/app/components/App.jsx
+++ kanban_app/app/components/App.jsx
@@ -29,7 +29,12 @@ export default class App extends React.Component {
   itemEdited(i, task) {
     var notes = this.state.notes;
 
-    notes[i].task = task;
+    if(task) {
+      notes[i].task = task;
+    }
+    else {
+      notes = notes.slice(0, i).concat(notes.slice(i + 1));
+    }
 
     this.setState({
       notes: notes
