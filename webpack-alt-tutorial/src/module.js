61fe1b1fa27f81e8a1829a3ac940c88aef8785c3
Ricardo Havill <rhavill@progresstesting.com>
2015-07-30 12:13:54 -0400

    Added simple webpack/alt example.

---
+++ webpack-alt-tutorial/src/module.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+React.render(  
+  <h1>React Example</h1>,
+  document.getElementById('content')
+);
---
+++ webpack-alt-tutorial/tmp/module.js
@@ -0,0 +1,9 @@
+var Example = Example || {}; Example["module"] =
+webpackJsonpExample__name_([1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	eval("'use strict';\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }\n\nvar _react = __webpack_require__(1);\n\nvar _react2 = _interopRequireDefault(_react);\n\n_react2['default'].render(_react2['default'].createElement(\n  'h1',\n  null,\n  'Example'\n), document.getElementById('content'));//@ sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9tb2R1bGUuanM/NmY1OCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnO1xuXG5SZWFjdC5yZW5kZXIoICBcbiAgPGgxPkV4YW1wbGU8L2gxPixcbiAgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2NvbnRlbnQnKVxuKTtcblxuXG5cbi8qKiBXRUJQQUNLIEZPT1RFUiAqKlxuICoqIC4vc3JjL21vZHVsZS5qc1xuICoqLyJdLCJtYXBwaW5ncyI6Ijs7OztBQUFBO0FBQ0E7OztBQUNBOzs7O0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ }
+]);
